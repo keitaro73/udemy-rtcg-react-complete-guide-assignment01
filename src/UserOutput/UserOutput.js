6d5eb1ad154e939bfeb69b944e403181c6ad20a4
Jason Johnston <jjohnston@smg.com>
2018-01-26 16:05:58 -0600

    section 3, lesson 46, assignment01 - base component setup

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = () => {
+    return (
+        <div>
+            <p>Welcome back, username!</p>
+            <p>The cake is a lie!</p>
+        </div>
+    );
+}
+
+export default userOutput;
