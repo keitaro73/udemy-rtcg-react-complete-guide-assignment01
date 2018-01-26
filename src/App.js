9934d774c8b36fecb7aa86639c8d41ac1a7bf85d
Jason Johnston <jjohnston@smg.com>
2018-01-26 16:14:34 -0600

    section 3, lesson 46, assignment01 - added state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
+  state = {
+    users: [
+      { userName: "harry.potter" },
+      { userName: "hermione.granger" }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <UserOutput userName="jsmith" />
-        <UserOutput userName="jdoe" />
+        <UserOutput userName={ this.state.users[0].userName } />
+        <UserOutput userName={ this.state.users[1].userName } />
       </div>
     );
   }
